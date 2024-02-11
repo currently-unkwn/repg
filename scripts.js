dee617991b322b2f06176a1e5e2a973704e3da04
Vladislav Kovac <vladkovac@Vladislavs-iMac.Dlink>
2024-02-11 17:04:18 +0200

    feat: Add UI for rep generator

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+import "./smoothscroll.min.js";
+
 // Max Lifts:
 //     Bench Press: 75
 //     Deadlift:
@@ -41,52 +43,50 @@
 
 // * or more for bench press
 
-// BENCH_PRESS_MAX = 75
-// DEADLIFT_MAX = 85
-// CHIN_UPS_MAX = 12
-const BENCH_PRESS_MAX = prompt("Barbell Bench Press Max:  ");
-const DEADLIFT_MAX = prompt("Deadlift Max:  ");
+// const BENCH_PRESS_MAX = prompt("Barbell Bench Press Max:  ");
+// const DEADLIFT_MAX = prompt("Deadlift Max:  ");
+
 // const BENCH_PRESS_MAX = 75;
 // const DEADLIFT_MAX = 85;
-const SQUAT_MAX = 0;
+// const SQUAT_MAX = 65;
 
-const Week1 = {
-  weekNumber: "WEEK 1",
+const week1 = {
+  id: 1,
   percentages: [72.5, 77.5, 82.5],
   reps: [8, 6, 4],
 };
 
-const Week2 = {
-  weekNumber: "WEEK 2",
+const week2 = {
+  id: 2,
   percentages: [75, 80, 85],
   reps: [7, 5, 3],
 };
 
-const Week3 = {
-  weekNumber: "WEEK 3",
+const week3 = {
+  id: 3,
   percentages: [77.5, 82.5, 90],
   reps: [6, 4, 2],
 };
 
-const Week4 = {
-  weekNumber: "WEEK 4",
+const week4 = {
+  id: 4,
   percentages: [80, 85, 95],
   reps: [5, 3, 1],
 };
 
-const Week5 = {
-  weekNumber: "WEEK 5",
+const week5 = {
+  id: 5,
   percentages: [50, 60],
   reps: [5, 5],
 };
 
-const Week11 = {
-  weekNumber: "WEEK 11",
+const week11 = {
+  id: 11,
   percentages: [80, 87.5, 92.5, 102, 104, 106],
   reps: [4, 2, 1, 1, 1, 1],
 };
 
-const Weeks = [Week1, Week2, Week3, Week4, Week5, Week11];
+const weeks = [week1, week2, week3, week4, week5, week11];
 
 //  Calculate weights for barbell, deadlifts and squats
 // E.g:
@@ -116,71 +116,198 @@ const Weeks = [Week1, Week2, Week3, Week4, Week5, Week11];
 //      (77.5%) 50.3kg — 6 reps
 //      (82.5%) 53.6kg — 4
 
-const calcWeights = function (percentagesArr, maxWeight) {
+/**
+ * *************************************************************
+ */
+
+const lifts = document.querySelector("#lifts");
+const form = document.querySelector("form[data-max-numbers]");
+const firstInput = form.querySelector("input");
+
+const KEY = "LIFTS";
+
+// Set focus on 1st input on load
+firstInput.focus();
+
+/**
+ * Get saved html from localStorage and render it on the page
+ */
+function init() {
+  const saved = localStorage.getItem(KEY);
+
+  if (!saved) return;
+
+  lifts.innerHTML = saved;
+}
+
+/**
+ * Calculates weight numbers from percentages
+ * @param {Array} percentagesArr
+ * @param {Number} maxWeight
+ * @returns {Array} New array with weight numbers
+ */
+function calcWeights(percentagesArr, maxWeight) {
+  // Convert  percentages into weight numbers
   return percentagesArr.map((percentage) => (percentage / 100) * maxWeight);
-};
+}
 
-const setWeights = function (weeksArr) {
-  weeksArr.forEach((week) => {
-    week.weightsBenchPress = calcWeights(week.percentages, BENCH_PRESS_MAX);
-    week.weightsDeadlifts = calcWeights(week.percentages, DEADLIFT_MAX);
-    week.weightsBarbellSquats = calcWeights(week.percentages, SQUAT_MAX);
+/**
+ * Calcs and sets weights and max lift numbers
+ * @param {Array} weeksArr
+ * @param {Object} maxWeights
+ * @returns {Array} New updated array with weights and max numbers
+ */
+function updateWeeks(weeksArr, maxWeights) {
+  // Get maxes
+  const { benchPressMax, squatMax, deadliftMax } = maxWeights;
+
+  // Set exercise weights and max numbers as properties on each week
+  return weeksArr.map((week) => {
+    return {
+      ...week,
+      weightsBenchPress: calcWeights(week.percentages, benchPressMax),
+      weightsDeadlifts: calcWeights(week.percentages, deadliftMax),
+      weightsBarbellSquats: calcWeights(week.percentages, squatMax),
+      benchPressMax,
+      squatMax,
+      deadliftMax,
+    };
   });
-};
+}
 
-setWeights(Weeks);
+/**
+ * Html markup for a single exercise
+ * @param {Object} args Object with properties
+ * @returns {HTMLElement} Html markup for a single exercise
+ */
+function renderLift(args) {
+  const { title, maxWeight, weights, week } = args;
 
-const exerciseWeightsAndRepsInAWeekOutput = function (
-  week,
-  exercise,
-  maxWeight
-) {
-  let output = "";
+  const isBenchPress = title === "Barbell Bench Press";
+  const lastRep = week.reps[week.reps.length - 1];
+  const isNotWeek5And11 = week.id !== 5 && week.id !== 11;
 
-  week.percentages.map((percentage, index) => {
-    const exerciseWeight = exercise[index].toFixed(1);
+  return `
+      <article class="lift">
+        <h3 class="exercise" data-exercise>${title}</h3>
+        <div class="max">Your max: <span data-max-lift>${maxWeight}kg</span></div>
 
-    output += `${index + 1} set — (${percentage}%) ${exerciseWeight}kg - ${
-      week.reps[index]
-    } reps \n`;
+        <table>
+          <thead>
+            <tr>
+              <th>Sets</th>
+              <th>Weights</th>
+              <th>Reps</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${weights
+              .map((weight, index) => {
+                return `
+                  <tr>
+                    <td class="set">${index + 1}</td>
+                    <td class="weight">
+                      <div class="weight-number">
+                        <span class="weight-percentage">${
+                          week.percentages[index]
+                        }%</span>
+                        ${weight.toFixed(1)}kg
+                      </div>
+                    </td>
+                    <td class="rep">${
+                      isBenchPress &&
+                      week.reps[index] === lastRep &&
+                      isNotWeek5And11
+                        ? week.reps[index] + " and more"
+                        : week.reps[index]
+                    }</td>
+                  </tr>
+              `;
+              })
+              .join("")}
+          </tbody>
+        </table>
+    </article>
+  `;
+}
+
+/**
+ * Html for all exercises in a week
+ * @param {Object} week
+ * @returns {HTMLElement} Html markup for lift content
+ */
+function renderLiftContent(week) {
+  const barbellBenchPressHtml = renderLift({
+    title: "Barbell Bench Press",
+    maxWeight: week.benchPressMax,
+    weights: week.weightsBenchPress,
+    week,
   });
 
-  return `Your max: ${maxWeight}kg \n${output}`;
-};
-
-const printAllExercisesWeightsAndRepsInAWeek = function (week) {
-  const benchPressOutput =
-    exerciseWeightsAndRepsInAWeekOutput(
-      week,
-      week.weightsBenchPress,
-      BENCH_PRESS_MAX
-    ) + "or more \n";
-
-  const deadliftOutput = exerciseWeightsAndRepsInAWeekOutput(
+  const barbellSquatHtml = renderLift({
+    title: "Barbell Squat",
+    maxWeight: week.squatMax,
+    weights: week.weightsBarbellSquats,
     week,
-    week.weightsDeadlifts,
-    DEADLIFT_MAX
-  );
+  });
 
-  const barbellSquatOutput = exerciseWeightsAndRepsInAWeekOutput(
+  const deadliftsHtml = renderLift({
+    title: "Deadlifts",
+    maxWeight: week.deadliftMax,
+    weights: week.weightsDeadlifts,
     week,
-    week.weightsBarbellSquats,
-    SQUAT_MAX
+  });
+
+  return `
+    <div class="lift-content">
+      <div class="lift-week">Week ${week.id}</div>
+      ${barbellBenchPressHtml}
+      ${barbellSquatHtml}
+      ${deadliftsHtml}
+    </div>
+  `;
+}
+
+/**
+ * Calculates and shows lift content on a page
+ * @param {Object} event object
+ */
+function handleSubmit(event) {
+  event.preventDefault();
+
+  // Get max lifts from form
+  let { benchPressMax, squatMax, deadliftMax } = Object.fromEntries(
+    new FormData(event.target)
   );
 
-  const output = `${week.weekNumber} \nBarbell Bench Press: \n${benchPressOutput} \nDeadlifts: \n${deadliftOutput} \nBarbell Squats: \n${barbellSquatOutput} \n`;
+  // If no value do nothing
+  if (!benchPressMax || !squatMax || !deadliftMax) return;
 
-  return output;
-};
+  // Convert values into a number
+  benchPressMax = parseFloat(benchPressMax);
+  squatMax = parseFloat(squatMax);
+  deadliftMax = parseFloat(deadliftMax);
 
-const printTotalWeightsAndReps = function (weeksArr) {
-  let total = "";
+  // Set weights properties
+  const updatedWeeks = updateWeeks(weeks, {
+    benchPressMax,
+    squatMax,
+    deadliftMax,
+  });
 
-  weeksArr.forEach(
-    (week) => (total += printAllExercisesWeightsAndRepsInAWeek(week))
-  );
+  // Render lifts content
+  const html = updatedWeeks.map((week) => renderLiftContent(week)).join("");
+  lifts.innerHTML = html;
 
-  return total;
-};
+  // Save lifts content to localStorage
+  localStorage.setItem(KEY, html);
 
-alert(printTotalWeightsAndReps(Weeks));
+  // Reset input values
+  form.querySelectorAll("input").forEach((input) => (input.value = 0));
+
+  // Scroll to lifts section
+  lifts.scrollIntoView({ behavior: "smooth" });
+}
+
+init();
+form.addEventListener("submit", handleSubmit);
